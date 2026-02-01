6ff99425eda97c573ead0a67d1858c24a263a43b
matheusvibe <157317252+mathfews@users.noreply.github.com>
2026-02-01 11:13:26 -0300

    add theme variation based on the result

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const values = document.querySelectorAll(".valueSum")
-const result = document.getElementById("result")
+const score = document.getElementById("score")
+const rating = document.getElementById("rating")
+const container = document.getElementById("container")
+const circle = document.getElementById("circle")
+const button = document.getElementById("continue")
 let sum = 0
 fetch("data.json")
 .then(response => response.json())
@@ -8,5 +12,19 @@ fetch("data.json")
         values[x].innerHTML = data[x].score 
         sum += data[x].score
     }
-    result.innerHTML = Math.trunc(sum / data.length)
+    score.innerHTML = Math.trunc(sum / data.length)
+    const result = Number(score.innerHTML)
+    console.log(result)
+    if (result <= 80 && result >= 60) {
+        document.documentElement.setAttribute("data-theme", "great")
+        rating.innerText = "Great"
+    } else if (result < 80 && result >= 50) {
+        document.documentElement.setAttribute("data-theme", "avarage")
+        rating.innerText = "Avarage"
+        container.style.color = "black"
+
+    } else if (result <  50) {
+        document.documentElement.setAttribute("data-theme", "bad")
+        rating.innerText = "Bad"
+    }
 })
